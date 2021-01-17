dccf94a191953afbbee631051b499a70846a7d42
mroot <almatov.ms@gmail.com>
2021-01-17 16:06:43 +0500

    start implement TourList

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
-import Tour from '../Tour';
+import React, { Component } from "react";
+import "./tourlist.scss";
+import Tour from "../Tour";
+import { tourData } from "../../tourData";
 
 export default class TourList extends Component {
-    render() {
-        return (
-            <div>
-                Hello from Tour List
-                <Tour />
-            </div>
-        )
-    }
+  state = {
+    tours: tourData,
+  };
+
+  removeTour = (id) => {
+    console.log(id);
+  };
+
+  render() {
+    const { tours } = this.state;
+
+    return (
+      <section className="tourlist">
+        {tours.map((tour) => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>
+        ))}
+      </section>
+    );
+  }
 }
