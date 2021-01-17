dfdee1d54996120c7424bcc265b1efad33f8f2e0
mroot <almatov.ms@gmail.com>
2021-01-17 15:35:52 +0500

    create Tour related files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import React, { Fragment } from "react";
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
 
 function App() {
   return (
     <Fragment>
       <Navbar />
+      <TourList />
     </Fragment>
   );
 }
